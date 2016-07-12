5b71dedfbf7b3d5638ff0c7ef21e9aa85b5c05ef
Ygarr <Urbanterror1>
2016-07-12 03:32:56 +0300

    homework comit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int n = 2345;
+	    int n = 2349;
+        int nn = 2350;
+
 
         if ( (n%2) == 0 ) {
             System.out.println("Число n: " + n + " - EVEN - Четное");
         } else {
             System.out.println("Число n: " + n + " - ODD - Нечетное");
         }
+
+        System.out.println("А число " + nn + ((nn & 1) == 0 ? " четное " : " нечетное "));//Don't know why
     }
 }
