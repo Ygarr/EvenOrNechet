6d7f9a49a328eee40a99ab8b6f450f49c23daef3
Ygarr <Urbanterror1>
2016-07-12 03:26:20 +0300

    homework commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int n = 2345;
+
+        if ( (n%2) == 0 ) {
+            System.out.println("Число n: " + n + " - EVEN - Четное");
+        } else {
+            System.out.println("Число n: " + n + " - ODD - Нечетное");
+        }
+    }
+}
